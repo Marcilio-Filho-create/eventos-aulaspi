8c96b07ff4747d8e783e8b6a885e492079537305
Marcílio-Filho-Create <marciliomtfilho6@gmail.com>
2021-09-30 17:45:08 -0300

    Update EventosController.java
    Exercício da Atividade 202 - Spring - Model, View e Controller.

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -3,12 +3,23 @@ package ifrn.pi.eventos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.eventos.models.Evento;
+
 @Controller
 public class EventosController {
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+	@RequestMapping("/eventos/confirma")
+	public String submit(Evento evento) {
+		System.out.println("O método foi executado");
+		System.out.println(evento.getNome());
+		System.out.println(evento.getLocal());
+		System.out.println(evento.getData());
+		System.out.println(evento.getHorario());
+		return "confirmButton";
 	}
 	
 }
